1ca2671c2768694cc9f25cef0e3c75d8774edb5b
Labha09 <labhabissa@ayopop.com>
2020-12-21 23:37:02 +0530

    moreassertions

--- test/brs/dev/feature/getdetails.spec.ts
+++ test/brs/dev/feature/getdetails.spec.ts
@@ -6,9 +6,9 @@ let jwtToken:string;
 
 describe('Testing on Biller Dashboard', async () => {
 
-    let token: string;//store jwt token
+    let jwtToken: string;//store jwt token
     let didLogIn: boolean = false;//check if user got logged in : use this to skip irrelevant tests
-
+    
     it('should call login api and return status code 200', async function () {
 
         let resp: any = await Helper.callAPI(config.apiCollections.login);
@@ -25,38 +25,17 @@ describe('Testing on Biller Dashboard', async () => {
 
         it('should call User Details api and return status code 200', async function () {
         //User Details API call
-        let stub = {
-            "success": true,
-            "statusCode": 200,
-            "message": "Partner user account details.",
-            "data": {
-                "HasBRS": true,
-                "HasMonitoring": true,
-                "HasTransactions": true,
-                "createdAt": "2019-10-10 03:47:33",
-                "email": "don@ayopop.com",
-                "id": 43,
-                "name": "Don",
-                "partnerID": 27,
-                "partnerName": "Ayopop test2",
-                "permissions": [
-                    "biller-dashboard.inquiries.list",
-                    "biller-dashboard.accounts.list",
-                    "biller-dashboard.transactions.download",
-                    "biller-dashboard.transactions.list",
-                    "biller-dashboard.users.create",
-                    "biller-dashboard.users.list",
-                    "biller-dashboard.users.update"
-                ],
-                "status": 1
-            }
-        };
         let apiOptions = config.apiCollections.getdetails;
         apiOptions.headers.Authorization+= jwtToken;
 
         let userLogResponse: any = await Helper.callAPI(config.apiCollections.getdetails);
         console.log("User Details response");
         console.log(userLogResponse);
+        //expect(userLogResponse.data.message).to.be.equals("Partner user account details.");
+        expect(userLogResponse).to.be.not.equals(null);
+        expect(userLogResponse.statusCode).to.be.equals(200);
+        expect(userLogResponse.data.token).to.be.not.equals(null);
+
         });
 
 
@@ -65,37 +44,42 @@ describe('Testing on Biller Dashboard', async () => {
 
         let apiOptions = config.apiCollections.deleteidpel;
         apiOptions.headers.Authorization+= jwtToken;
-
+        
         let deleteApiResponse: any = await Helper.callAPI(config.apiCollections.deleteidpel);
         console.log("Delete Account response");
         console.log(deleteApiResponse);
+       // expect(deleteApiResponse.data.message).to.be.equals("Kamu tidak bisa menghapus IDPEL ini sekarang untuk alasan keamanan data.\n\t\tSilahkan hubungi Tim Ayoconnect untuk tindak lanjut");
+        expect(deleteApiResponse).to.be.not.equals(null);
+        expect(deleteApiResponse.statusCode).to.be.equals(200);
+        expect(deleteApiResponse.data.token).to.be.not.equals(null);
         });
         
         it('should call User Accounts api and return status code 200', async function () {
             //Get User Accounts API call
-         //   "statusCode": 200,
-         //   "message": "Account information fetched successfully."
-    
             let apiOptions = config.apiCollections.userslist;
             apiOptions.headers.Authorization+= jwtToken;
     
             let userApiResponse: any = await Helper.callAPI(config.apiCollections.userslist);
             console.log("User Accounts response");
             console.log(userApiResponse);
+           // expect(userApiResponse.data.message).to.be.equals("Account information fetched successfully.");
+            expect(userApiResponse).to.be.not.equals(null);
+            expect(userApiResponse.statusCode).to.be.equals(200);
+            expect(userApiResponse.data.token).to.be.not.equals(null);
         });
 
         it('should call User update api and return status code 200', async function () {
             //User Update API call
-           // "success": true,
-           // "statusCode": 200,
-           // "message": "Biller User created successfully."
-    
             let apiOptions = config.apiCollections.userupdate;
             apiOptions.headers.Authorization+= jwtToken;
     
             let userupdateApiResponse: any = await Helper.callAPI(config.apiCollections.userupdate);
             console.log("User Update response");
             console.log(userupdateApiResponse);
+            //expect(userupdateApiResponse.data.message).to.be.equals("Biller User created successfully.");
+            expect(userupdateApiResponse).to.be.not.equals(null);
+            expect(userupdateApiResponse.statusCode).to.be.equals(200);
+            expect(userupdateApiResponse.data.token).to.be.not.equals(null);
         });
 
     });
